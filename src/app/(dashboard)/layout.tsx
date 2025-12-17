e73e80679e705861e3728e81e2a031ce91bafb32
Purbanchal Hasta udyog <purbanchalhastaudyog@gmail.com>
2025-12-17 06:49:36 +0000

    in subs page when im scrol too many blank area appear why

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
     <SidebarProvider>
       <div className="min-h-screen w-full bg-background text-foreground">
         <AppSidebar />
-        <div className="flex flex-col md:pl-[var(--sidebar-width)] group-data-[collapsible=icon]:md:pl-[var(--sidebar-width-icon)] transition-[padding] ease-linear h-screen">
+        <div className="flex flex-col md:pl-[var(--sidebar-width)] group-data-[collapsible=icon]:md:pl-[var(--sidebar-width-icon)] transition-[padding] ease-linear">
           <Header />
           <main className="p-4 sm:p-6 lg:p-8 overflow-auto">
               {children}
