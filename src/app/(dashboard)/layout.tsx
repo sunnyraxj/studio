b69ccb8d3ccd1a3e89711b232c6c1d06bb82aa4e
Purbanchal Hasta udyog <purbanchalhastaudyog@gmail.com>
2025-12-17 07:12:10 +0000

    remove unwanted scrol make page height as same as content display

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
         <AppSidebar />
         <div className="flex flex-col md:pl-[var(--sidebar-width)] group-data-[collapsible=icon]:md:pl-[var(--sidebar-width-icon)] transition-[padding] ease-linear">
           <Header />
-          <main className="h-[calc(100vh-theme(height.16))] overflow-y-auto p-4 sm:p-6 lg:p-8">
+          <main className="min-h-[calc(100vh-theme(height.16))] p-4 sm:p-6 lg:p-8">
               {children}
           </main>
         </div>
