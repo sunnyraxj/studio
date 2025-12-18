d8728619f8539cd8699ba6e54f14d578f116e3d9
Purbanchal Hasta udyog <purbanchalhastaudyog@gmail.com>
2025-12-18 10:18:06 +0000

    in admin panel side bar and main section overlap

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { FirebaseClientProvider, useMemoFirebase } from '@/firebase';
+import { FirebaseClientProvider } from '@/firebase';
 import {
   Sidebar,
   SidebarHeader,
