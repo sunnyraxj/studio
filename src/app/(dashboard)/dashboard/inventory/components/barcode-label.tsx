aa133041e6d20ff8e8a34aef0b7a2245873872ee
Purbanchal Hasta udyog <purbanchalhastaudyog@gmail.com>
2026-01-10 10:41:55 +0000

    make shop in in barcode lebel make smaller the product name

--- src/app/(dashboard)/dashboard/inventory/components/barcode-label.tsx
+++ src/app/(dashboard)/dashboard/inventory/components/barcode-label.tsx
@@ -25,15 +25,15 @@ export const BarcodeLabel: React.FC<BarcodeLabelProps> = ({ item, shopName, isQu
             style={{ width: '2.5in', height: '1.5in', boxSizing: 'border-box' }}
         >
             <div className="w-full flex justify-center items-center relative top-[2%]">
-                <p className="text-xs uppercase">{shopName}</p>
+                <p className="text-[10px] uppercase">{shopName}</p>
                 {isQuickBarcode && (
                     <span className="absolute right-0 text-[8px] uppercase font-bold">Quick</span>
                 )}
             </div>
             
             <div className="w-full flex-grow flex flex-col justify-center items-center gap-0">
-                 <p className="text-base truncate max-w-full relative top-[-10%]">{item.name}</p>
-                <div className="flex flex-col items-center relative top-[-10%] pb-1">
+                 <p className="text-sm truncate max-w-full relative top-[-10%]">{item.name}</p>
+                <div className="flex flex-col items-center relative top-[-5%] pb-1">
                     <Barcode 
                         value={item.sku || 'N/A'}
                         width={1.2}
@@ -50,7 +50,7 @@ export const BarcodeLabel: React.FC<BarcodeLabelProps> = ({ item, shopName, isQu
                     MRP: <IndianRupee className="h-3 w-3 mx-0.5" />
                     {(item.price || 0).toLocaleString('en-IN', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
                 </div>
-                <div className='text-sm flex items-center justify-center gap-2 w-full px-1 font-extrabold'>
+                <div className='text-[10px] flex items-center justify-center gap-2 w-full px-1 font-extrabold'>
                     {item.size && <div>Size: {item.size}</div>}
                     {item.expiryDate && <div>Expiry: {format(new Date(item.expiryDate), 'd/M/yy')}</div>}
                 </div>
