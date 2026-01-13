2028c350d24905f2e134e9ee45d1f8824b5290b8
Purbanchal Hasta udyog <purbanchalhastaudyog@gmail.com>
2026-01-13 15:56:13 +0000

    remove header from my website i dont need header

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -406,12 +406,6 @@ export default function DashboardLayout({
     <SidebarProvider>
       <AppSidebar shopName={shopName} isExpired={isUIBlocked} />
       <SidebarInset>
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6 sticky top-0 z-30">
-            <MobileSidebar shopName={shopName} isExpired={isUIBlocked} />
-            <div className="w-full flex-1">
-              <UniversalSearch navLinks={navLinks} />
-            </div>
-        </header>
         <main className={cn("flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6", isUIBlocked && "pointer-events-none opacity-50")}>
             {children}
         </main>
